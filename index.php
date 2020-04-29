392449ac2a1c348e9864a428ad804d26624f0f7f
rheinschiene <github@ms86.de>
2020-04-29 10:27:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
+<html>
+  <head>
+    <title>BBk Test Project</title>
+  </head>
+  <body>
+    <h1>Test</h1>
+    <p>
 <?php
+$servername = "localhost";
+$username = getenv("dbuser");
+$password = getent("dbpw");
+echo getenv("MYSQL_SERVICE_HOST");
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-phpinfo();
-
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
+    </p>
+  </body>
+</html>
